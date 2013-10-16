022e9a7a66ef081fd84153068064496ba2fbbfe7
fdreyfus <frederic.dreyfus@gmail.com>
2013-10-16 13:02:32 +0200

    Added GAE servlet

--- tuning-datefield-demo/src/main/java/org/vaadin/addons/tuningdatefield/demo/DemoWebAppInitializer.java
+++ tuning-datefield-demo/src/main/java/org/vaadin/addons/tuningdatefield/demo/DemoWebAppInitializer.java
@@ -10,19 +10,19 @@ import javax.servlet.ServletRegistration;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import com.vaadin.server.GAEVaadinServlet;
 import com.vaadin.server.VaadinServlet;
 
 // web.xml because of google appengine 
 //@WebListener
 public class DemoWebAppInitializer implements ServletContextListener {
-    
+
     public static final String PRODUCTION_MODE = Boolean.toString(true);
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext context = sce.getServletContext();
-        ServletRegistration.Dynamic vaadinServletRegistration = context.addServlet("DemoServlet",
-                MyServlet.class);
+        ServletRegistration.Dynamic vaadinServletRegistration = context.addServlet("DemoServlet", MyServlet.class);
         vaadinServletRegistration.addMapping("/*");
         vaadinServletRegistration.setAsyncSupported(true);
         vaadinServletRegistration.setLoadOnStartup(10);
@@ -32,17 +32,27 @@ public class DemoWebAppInitializer implements ServletContextListener {
                 "org.vaadin.addons.tuningdatefield.demo.widgetset.TuningDateFieldDemoWidgetset");
 
     }
-    
-    // extends GAEVaadinServlet for GAE deployment
+
     public static class MyServlet extends VaadinServlet {
-        
+
         private static final long serialVersionUID = 2122520422024963397L;
-        
+
         @Override
         public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
             super.service(req, res);
         }
-        
+
+    }
+
+    public static class MyGAEServlet extends GAEVaadinServlet {
+
+        private static final long serialVersionUID = 2122520422024963397L;
+
+        @Override
+        public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+            super.service(req, res);
+        }
+
     }
 
     @Override
